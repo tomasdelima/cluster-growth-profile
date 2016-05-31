24e265352871b056f04c48abf6fea15115cb016f
Tomás de Lima <tomasdelima@gmail.com>
2016-05-31 11:11:30 -0300

    Includes some fields

--- app/assets/javascripts/cluster_growth.js
+++ app/assets/javascripts/cluster_growth.js
@@ -2,4 +2,12 @@ app.controller('formController', function ($scope, $http, $mdDialog) {
   $scope.logout = function (route) {
     $http.delete(route).then(function () {location.reload()}, function () {location.reload()})
   }
+
+  $scope.sendForm = function () {
+    $http.post('/cluster_growth', $scope.form).then(function () {
+      console.log('Success')
+    }, function () {
+      console.log('Error')
+    })
+  }
 })
