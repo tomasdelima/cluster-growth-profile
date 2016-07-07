dac1f9ba462c3cb982c425750ebe7635992b2ebf
Tomás de Lima <tomasdelima@gmail.com>
2016-07-07 15:37:33 -0300

    Creates the city model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615193330) do
+ActiveRecord::Schema.define(version: 20160707183551) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at",                          null: false
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20160615193330) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "cluster_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "cities", ["cluster_id"], name: "index_cities_on_cluster_id"
 
   create_table "clusters", force: :cascade do |t|
     t.string   "name"
